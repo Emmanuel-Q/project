25027a3c4d2087cd863203b52899dd687359dde0
Emmanuel-Q <78229107+Emmanuel-Q@users.noreply.github.com>
2023-04-29 15:22:26 +0000

    add templates and admin files

---
+++ admin/authenticate.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'projectdb';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+//Check if the data from the login form was submitted.
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	exit('Please fill both the username and password fields!');
+}
+
+// Prepare our SQL statement to prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+
+	
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+
+	// Store the result
+	$stmt->store_result();
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        if ($_POST['password'] === $password) { 
+            // Creates sessions
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            header('Location: home.php');
+        } else {
+            // Incorrect password
+            echo 'Incorrect username and/or password!';
+        }
+    } else {
+        // Incorrect username
+        echo 'Incorrect username and/or password!';
+    }
+
+	$stmt->close();
+}
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include 'tmpl/header.php' ?>
+
+<?php include 'tmpl/carousel.php' ?>
+
+<?php include 'tmpl/about.php' ?>
+   
+<?php include 'tmpl/services.php' ?>
+
+<?php include 'tmpl/team.php' ?>
+
+<?php include 'tmpl/testimonial.php' ?>
+
+<?php include 'tmpl/footer.php' ?>
